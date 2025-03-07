9d595670b6dfcd1675c1debb166ebfa09f43cf2d
Reza P <reza.pahlava@stella-ars.sch.id>
2025-03-07 08:56:13 +0000

    update sql

--- sis/settings.py
+++ sis/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'sis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sis',
+        'USER': 'sis_user',
+        'PASSWORD': 'Stellamaris',
+        'HOST': '172.17.0.32',
+        'PORT': '5433',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
